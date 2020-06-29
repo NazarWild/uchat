cae84214e625a98a4956f13b2642f830a058e552
Oleksii Kolevatov <okolevatov@e1r10p3.unit.ua>
2020-06-29 19:50:33 +0300

    Server Update 1.1

--- server/src/mx_delete.c
+++ server/src/mx_delete.c
@@ -4,18 +4,20 @@ static void deleteing_request(cJSON *root) {
     cJSON *delete = cJSON_GetObjectItemCaseSensitive(root, "ACCOUNT");
     char *str2 = NULL;
     
-    asprintf(&str2, "login = %s", delete->valuestring);
-    mx_set_value("persons_id", "pass = DELETE", str2);
-    mx_set_value("persons_id", "login = DELETE", str2);
+    asprintf(&str2, "login = '%s'", delete->valuestring);
+    mx_set_value("persons_id", "pass = 'DELETE'", str2);
+    mx_set_value("persons_id", "login = 'DELETE'", str2);
     free(str2);
 } 
-
-void mx_delete(int fd, cJSON *root) {
+bool mx_delete(int fd, cJSON *root) {
     cJSON *delete = cJSON_GetObjectItemCaseSensitive(root, "DELETE");
+    int exit;
 
     if (cJSON_IsTrue(delete) == 1) {
         //zapros na udalenie danih SQL
         deleteing_request(root);
         write(fd, "Your account was succesful deleted", 35);//pishem chto account udachno udalen
+        return true;
     }
+    return false;
 }
