f5ecfecb7a257fada26d3d76f537d79078b5e370
Oleksii Kolevatov <okolevatov@e1r9p5.unit.ua>
2020-07-05 17:05:26 +0300

    STRUCT FOR ONLINE

--- server/src/mx_whoonline.c
+++ server/src/mx_whoonline.c
@@ -3,6 +3,8 @@
 static void send_online(cJSON *ON, int fd) {
     char *string = cJSON_Print(ON);
 
+    write(1, string, strlen(string));
+    exit(0);
     write(fd, string, strlen(string));
     free(string);
 }
@@ -23,13 +25,26 @@ static void adding_sys(cJSON *root) {
 
     is_online = cJSON_CreateTrue();
 
-    cJSON_AddItemToObject(root,"ONLINE", is_online);
+    cJSON_AddItemToObject(root,"USERS", is_online);
+}
+
+static void adding_param(cJSON *online, t_online *arr_users) {
+    cJSON *user_id = NULL;
+    cJSON *online_bool = NULL;
+
+    user_id = cJSON_CreateString( arr_users->id);
+    cJSON_AddItemToObject(online, "user_id", user_id);
+    if (arr_users->online == true) 
+        online_bool = cJSON_CreateTrue();
+    else 
+        online_bool = cJSON_CreateFalse();
+    cJSON_AddItemToObject(online, "online", online_bool);
 }
 
 void mx_whoonline(use_mutex_t *mutex) {
     t_list *online_struct = mx_where_not_1(mutex);
     cJSON *on = cJSON_CreateObject();
-    cJSON *who_online = cJSON_CreateArray();
+    cJSON *users = cJSON_CreateArray();
     cJSON *online = NULL;
     cJSON *str = NULL;
 
@@ -38,13 +53,14 @@ void mx_whoonline(use_mutex_t *mutex) {
     int count_of = count_of_struct(online_struct);
     adding_sys(on);
 
-    cJSON_AddItemToObject(on, "who_online", who_online);
+    cJSON_AddItemToObject(on, "user", users);
     for (int i = 0; i < count_of; i++) {
         struco = (t_online*) online_struct->data;
         online = cJSON_CreateObject();
-        cJSON_AddItemToArray(who_online, online);
+        cJSON_AddItemToArray(users, online);
         str = cJSON_CreateString( struco->login);
-        cJSON_AddItemToObject(online, "online", str);
+        cJSON_AddItemToObject(online, "login", str);
+        
         online_struct = online_struct->next;
     }
     send_online(on, mutex->cli_fd);
