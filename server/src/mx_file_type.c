35b682fabdf8d480c4eb05da962c762d093ba5e8
Oleksii Kolevatov <okolevatov@e1r9p5.unit.ua>
2020-07-20 15:52:27 +0300

    V 0.11

--- server/src/mx_file_type.c
+++ server/src/mx_file_type.c
@@ -1,36 +1,48 @@
 #include "../inc/uchat.h"
 
-static void creating(cJSON* TYPE, cJSON* BYTES, cJSON* MESS, t_use_mutex *mutex) {
-    char *path = mx_strjoin("new_file.", TYPE->valuestring); //пусть отправляет название с точкой в конце
+static char *creating(cJSON* TYPE, cJSON* BYTES, cJSON* MESS, t_use_mutex *mutex) {
+    char *tmp = mx_strjoin(MESS->valuestring, TYPE->valuestring);
+    char *path =  mx_strjoin("file_serv/", tmp);//пусть отправляет название с точкой в конце
     int stream = open(path, O_RDWR | O_CREAT | O_APPEND, S_IRWXU);
-    printf("bytes: %d\n", BYTES->valueint);
     char *buff = (char *)malloc(sizeof(char) * BYTES->valueint);
-    // int a;
 
-    // while (a += read(mutex->cli_fd, buff, BYTES->valueint)) {
-    //     write(stream, buff, BYTES->valueint);
-    //     if (a >= BYTES->valueint)
-    //         break;
-    // }
     recv(mutex->cli_fd, buff, BYTES->valueint, MSG_WAITALL);
     write(stream, buff, BYTES->valueint);
-    
-    write(1, "FILE DONE", 9);
     close(stream);
-    free(path);
     free(buff);
+    free(tmp);
+    return path;
+}
+
+static void send_cj(cJSON *root, t_use_mutex *mutex, char *name) {
+    char *str = NULL;
+    char *path = mx_strjoin("file_serv/", name);
+    int fd = open(path, O_RDWR);
+    cJSON* CHAT_ID = cJSON_GetObjectItemCaseSensitive(root, "CHAT_ID");
+    cJSON* TO = cJSON_GetObjectItemCaseSensitive(root, "TO");
+    cJSON* BYTES = cJSON_GetObjectItemCaseSensitive(root, "BYTES");
+
+    asprintf(&str, "{\"FROM\":%d,\"MESS\":%s,\"TYPE\":%s,\"CHAT_ID\":%s}\n", mutex->user_id, path, "file",CHAT_ID->valuestring);
+    write(atoi(TO->valuestring), str, strlen(str));
+    free(str);
+    str = (char *) malloc(sizeof(char) * BYTES->valueint);
+    read(fd, str, BYTES->valueint);
+    write(atoi(TO->valuestring), str, BYTES->valueint);
+    free(str);
 }
 
 void mx_file_type(cJSON *root, t_use_mutex *mutex) { 
-    cJSON* TO = cJSON_GetObjectItemCaseSensitive(root, "TO");
     cJSON* MESS = cJSON_GetObjectItemCaseSensitive(root, "MESS");
     cJSON* TYPE = cJSON_GetObjectItemCaseSensitive(root, "TYPE");
     cJSON* BYTES = cJSON_GetObjectItemCaseSensitive(root, "BYTES");
+    cJSON* CHAT_ID = cJSON_GetObjectItemCaseSensitive(root, "CHAT_ID");
+    char *path = NULL;
 
-    if (cJSON_IsString(TO) && (TO->valuestring != NULL)
-        && cJSON_IsString(MESS) && (MESS->valuestring != NULL)
-        && strcmp("text", TYPE->valuestring) != 0) {
-        creating(TYPE, BYTES, MESS, mutex);
+    if (strcmp("file", TYPE->valuestring) == 0) {
+        path = creating(TYPE, BYTES, MESS, mutex);
+        //mx_add_message(atoi(CHAT_ID->valuestring), path, 1, mutex);
+        send_cj(root, mutex, path);
     }
+    free(path);
 }
 
