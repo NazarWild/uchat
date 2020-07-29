db9bceb85eb9d2e3f57537a9f4928eeacf9a3561
Dariia Mushynska <dmushynska@e1r7p5.unit.ua>
2020-07-29 21:55:47 +0300

    fix

--- server/src/mx_send_mess.c
+++ server/src/mx_send_mess.c
@@ -10,7 +10,6 @@ static void send_mess(int to, char *mess, int chat_id, t_use_mutex *mutex) {
         type = strdup("sticker");
     else 
         type = strdup("text");
-    write(1, mx_itoa(chat_id), strlen(mx_itoa(chat_id)));
     asprintf(&new, "{\"IF_MESS\":true,\"FROM\":%d,\"MESS\":\"%s\",\"CHAT_ID\":%d,\"TYPE\":\"%s\"}\n", mutex->user_id, mess, chat_id, type);
     mx_send_user_with_dif_sock(mutex, to, new, strlen(new));
     stickers = 0;
